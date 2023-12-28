acae057a2392f89b18b6ac553628a024dc246c7e
Jan Tusil <jenda.tusil@gmail.com>
2023-12-28 16:34:42 +0100

    Use clang-tidy. Implement some Core Guidelines suggestions.

--- build.sh
+++ build.sh
@@ -3,22 +3,24 @@
 
 build_configuration() {
   CONFIG="$1"
+  shift
   echo "Building $CONFIG"
   BUILD_DIR="$(pwd)/build/$CONFIG"
   CONFIG_ABS_PATH="$(pwd)/build-configurations/$CONFIG"
   echo "BUILD_DIR: $BUILD_DIR"
   mkdir -p "$BUILD_DIR"
   pushd "$BUILD_DIR"
-  env VERBOSE=1 make --file "$CONFIG_ABS_PATH/Makefile" BUILDROOT="$BUILD_DIR" --debug
+  env VERBOSE=1 make --file "$CONFIG_ABS_PATH/Makefile" BUILDROOT="$BUILD_DIR" $@
   popd
 }
 
 CONFIG="$1"
+shift
 if [[ -z "$CONFIG" ]] then
   for CONFIG_DIR in build-configurations/*; do
-    build_configuration "$(basename "$CONFIG_DIR")"
+    build_configuration "$(basename "$CONFIG_DIR")" $@
   done
 else
-  build_configuration "$CONFIG"
+  build_configuration "$CONFIG" $@
 fi
 
