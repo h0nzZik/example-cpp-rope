bbe00a734efb2dce6cd6f72d8715729dff9fb02b
Jan Tusil <jenda.tusil@gmail.com>
2023-12-28 16:51:43 +0100

    A trivial test (but clang-only).

--- librope/test_rope.cxx
+++ librope/test_rope.cxx
@@ -2,21 +2,25 @@
 
 import rope;
 
-//import <catch2/catch_test_macros.hpp>
-//import <catch2/catch_all.hpp>;
-//#include <catch2/catch_all.hpp>
-
-//#include <catch2/catch_all.hpp>
-//#include <cstddef>
-//#include <doctest/doctest.h>
-//#include <iostream>
-
 
 TEST_CASE("trivial") {
     REQUIRE(true);
 }
 
+#if defined(__GNUC__) && !defined(__clang__)
+    // The compilation crashes with g++ (GCC) 13.2.0
+    #warning("Compiling with GCC, disabling some tests due to bugs in GCC's RTL pass")
+#else
 
+TEST_CASE("simple conversions") {
+    auto const simple_conversion_property = [](std::string s) {
+        REQUIRE(std::string(Rope(s)) == s);
+    };
+
+    simple_conversion_property(std::string("I am a string!"));
+}
+
+#endif
 
 TEST_CASE("rope_something") {
     REQUIRE(3 == rope_something());
